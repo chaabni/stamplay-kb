f61094c6d13f57bfe33d3fea9198232a605ade21
isaiahgrey93 <isaiahgrey@gmail.com>
2015-10-20 15:28:25 -0700

    added slack invited

--- dist/javascripts/controllers/controllers.js
+++ dist/javascripts/controllers/controllers.js
@@ -127,9 +127,10 @@ angular.module("app")
 }])
 
 angular.module("app").controller("SlackInviteController", ["$scope", "$stamplay", function($scope, $stamplay) {
-    var slack_invite_hook = new Stamplay.Webhook('slack invite requests');
+    var invite = new $stamplay.Cobject('invite').Model;
     $scope.request_invite = function() {
-        slack_invite_hook.post({ email : $scope.email}).then(function (response) {
+        invite.set("email", $scope.email);
+        invite.save().then(function (response) {
             Materialize.toast("We will send an invite to, " + $scope.email + " shortly, thank you.", 3000)
         }, function(err){
             Materialize.toast(err, 2000)
